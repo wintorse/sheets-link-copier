d50bd1aa48a3f3455b4d9ed77b62423953e75241
Toma Ishii <tumaltofff@gmail.com>
2024-11-28 08:03:20 +0900

    separate types

---
+++ src/types/constants.ts
@@ -0,0 +1,14 @@
+import type { EmojiNames } from "./types";
+export const defaultEmojiNames: EmojiNames = {
+  googleSheets: ":google_sheets:",
+  googleDocs: ":google_docs:",
+  googleSlides: ":google_slides:",
+  googleDrive: ":google_drive_2:",
+  github: ":github:",
+  githubPullRequest: ":open_pull_request:",
+  githubIssue: ":open_issue:",
+  jiraIssue: ":jira:",
+  asanaTask: ":asana:",
+  backlogIssue: ":backlog:",
+  redmineTicket: ":redmine_ticket:",
+};
---
+++ src/types/types.ts
@@ -0,0 +1,13 @@
+export type EmojiNames = {
+  googleSheets: string;
+  googleDocs: string;
+  googleSlides: string;
+  googleDrive: string;
+  github: string;
+  githubPullRequest: string;
+  githubIssue: string;
+  jiraIssue: string;
+  asanaTask: string;
+  backlogIssue: string;
+  redmineTicket: string;
+};
